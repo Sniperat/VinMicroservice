43be90eccc680f37066faef409bf598294f076bb
sniperat <Sniperat2000@gmail.com>
2023-02-20 08:34:16 +0500

    forgot about migrations

--- main/tests.py
+++ main/tests.py
@@ -4,24 +4,5 @@ from django.db import connections, connection
 from django.db.utils import OperationalError
 import requests
 
-class TasksViewTest(TestCase):
-    
-    def test_database_url(self):
-        
-        connection.ensure_connection()
 
-    # def test_vin_decoder(self):
-
-    #     r = requests.get(settings.VIN_DECODER_URL+'SCBBR9ZA1AC063223kj/')
-    #     json_list = r.json()
-    #     # print(json_list)
-    #     self.assertTrue(json_list['decode']['status'] == 'SUCCESS')
-            # print('problem with VIN_DECODER_URL')
-
-
-
-    def test_endpoint_url(self):
-        self.client.post('/?vin=11111111111111111')
-
-
-        
+# I'm so sorry but a never wrote a test
