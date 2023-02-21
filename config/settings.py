d6f8ef68ea6ab5d1a39287719ccd3c682353c4a5
sniperat <Sniperat2000@gmail.com>
2023-02-21 16:30:01 +0500

    add something to test.py

--- config/settings.py
+++ config/settings.py
@@ -86,14 +86,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env('DATABASE_NAME'),
+#         "USER": env('DATABASE_USER'),
+#         "PASSWORD": env('DATABASE_PASS'),
+#         "HOST": env('DATABASE_HOST'),
+#         "PORT": env('DATABASE_PORT')
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env('DATABASE_NAME'),
-        "USER": env('DATABASE_USER'),
-        "PASSWORD": env('DATABASE_PASS'),
-        "HOST": env('DATABASE_HOST'),
-        "PORT": env('DATABASE_PORT')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,35 @@ from django.test import TestCase
 from django.conf import settings
 from django.db import connections, connection
 from django.db.utils import OperationalError
+from .models import DetailModel, WeightModel
 import requests
 
 
-# I'm so sorry but unfortunately a never work with a testing
+class Test(TestCase):
+
+    def test_model(self):
+
+        d = DetailModel.objects.create(
+            vin_id='ASD123ASD123ASD12',
+            year='2000',
+            make='make by test',
+            model='test model',
+            color='black',
+            dimensions={'test': 'test'}
+        )
+        self.assertTrue(isinstance(d, DetailModel))
+
+        w = WeightModel.objects.create(
+            detail=d,
+            _type='test',
+            unit='uzs',
+            value=2000
+        )
+        self.assertTrue(isinstance(w, WeightModel))
+
+    def test_api(self):
+
+        try:
+            self.client.post('/ASD123ASD123ASD12/')
+        except OperationalError as a:
+            print(a)
